68c6bd315c74f6eb03bc56aeef2e247f8e2f587d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:55:56 -0700

    i think i did great

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,24 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot doom = new Robot();
+		int sides=10;
+		int angles=360/sides;
+		for (int i = 0; i <sides;i++) {
+			doom.changeRobot(
+					"https://lafrikileria.com/21532-large_default/funko-pop-power-rangers-rojo-version-2017.jpg");
+			
+			doom.move(100);
+			doom.turn(-70);
+			doom.move(50);
+			doom.penUp();
+			doom.penDown();
+			doom.sparkle();
+
+		}
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -6,20 +6,23 @@ public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
+		Robot doom = new Robot();
 		// 5. Set your robot's pen down 
-		
+		doom.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		doom.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
+		for (int i = 0; i <75; i++) {
+			
 		
 			// 7. Change the pen color to random
-		
+		doom.setRandomPenColor();
 			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			doom.move(5*i);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+		doom.turn(360/7);
 			// 8. Set the pen width to i
-			
+		doom.setPenWidth(i);
+		}
 	}
 }
