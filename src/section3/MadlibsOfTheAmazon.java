dd10ceabb324b9d8238e01d859c87a8438cec1f1
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-09 11:57:54 -0700

    day4

--- src/section3/MadlibsOfTheAmazon.java
+++ src/section3/MadlibsOfTheAmazon.java
@@ -7,17 +7,24 @@ public class MadlibsOfTheAmazon {
 	public static void main(String[] args) {
 		// Put this sentence in a pop up:
 		// If you find yourself having to cross a piranha-infested river, here's how to do it...
-		
+		JOptionPane.showMessageDialog(null, "if you find yourself haviing to cross a piranha-infested river, here's how to do it");
 		// Get the user to enter an adjective
-
+		String adjective = JOptionPane.showInputDialog("adjective");
 		// Get the user to enter a type of liquid
-
+		String liquid = JOptionPane.showInputDialog("liquid");
 		// Get the user to enter a body part
-
+		String partString  = JOptionPane.showInputDialog("body part");
 		// Get the user to enter a verb
-
+		String verb = JOptionPane.showInputDialog("verb");
 		// Get the user to enter a place
-
+		String place = JOptionPane.showInputDialog("place");
+		JOptionPane.showMessageDialog(null, "\n" + 
+				"Piranhas are more "++" during the day, so cross the river at\n" + 
+				"night. Piranhas are attracted to fresh [type of liquid] and will most\n" + 
+				"likely take a bite out of your [body part] if you [verb]. Whatever\n" + 
+				"you do, if you have an open wound, try to find another way to get\n" + 
+				"back to the [place]. Good luck!");
+		
 		// Fit the user's words into this sentence, and save it in a String:
 		// Piranhas are more [adjective] during the day, so cross the river at
 		// night. Piranhas are attracted to fresh [type of liquid] and will most
--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,11 +6,15 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
-		// 2. If they say "yes", tell them they will rule the world.
-
+		String imput = JOptionPane.showInputDialog("can you code");
+		// 2. If they say "yesif(imput.equalsIgnoreCase("yes"))if(imput.equalsIgnoreCase("yes"))", tell them they will rule the world.
+if(imput.equalsIgnoreCase("yes")) {
+	JOptionPane.showInputDialog("you will rule the world");
+}
 		// 3. Otherwise, wish them good luck washing dishes.
+if(imput.equalsIgnoreCase("no"));
+JOptionPane.showInputDialog("good luck washing dishes");
 
 	}
-}
+	}
 
